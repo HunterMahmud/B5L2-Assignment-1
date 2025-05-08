6c9b9f0cd8d848a8e35a5308440bda9e9491bbec
huntermahmud <mahmudthedatahunter@gmail.com>
2025-05-08 19:27:03 +0600

    sovled: problem 5

--- index.ts
+++ index.ts
@@ -61,30 +61,46 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
  */
 
 class Vehicle {
-    private make: string;
-    private year: number;
-    constructor(make: string, year: number){
-        this.make = make;
-        this.year = year;
-    }
+  private make: string;
+  private year: number;
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
 
-    getInfo(){
-        return `Make: ${this.make}, Year: ${this.year}`
-    }
+  getInfo() {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
 }
 
-class Car extends Vehicle{
-    private model: string;
-    constructor(make: string, year: number, model: string){
-        super(make, year);
-        this.model = model;
-    }
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
 
-    getModel(){
-        return `Model: ${this.model}`;
-    }
+  getModel() {
+    return `Model: ${this.model}`;
+  }
 }
 
 // const myCar = new Car("Toyota", 2020, "Corolla");
 // console.log(myCar.getInfo());   // Output: "Make: Toyota, Year: 2020"
 // console.log(myCar.getModel());  // Output: "Model: Corolla"
+
+/**
+ * problem: 5
+ * @Solution
+ */
+
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+
+// console.log(processValue("hello")); // Output: 5
+// console.log(processValue(10));      // Output: 20
